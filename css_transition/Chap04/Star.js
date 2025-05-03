0962ffa4346196e8bbef12de823726b6bb87b6ed
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-05-03 19:41:51 +0900

    starfield 만들기 3

--- css_transition/Chap04/Star.js
+++ css_transition/Chap04/Star.js
@@ -1,9 +1,12 @@
 export default class Star {
-  constructor(number, x, y, z) {
+  constructor(number, x, y, scale) {
     const element = document.createElement("div");
     element.classList.add("star");
     element.innerHTML = number;
-    element.style.cssText = `left: ${x}px; top: ${y}px`;
+
+    element.style.left = `${x}px`;
+    element.style.top = `${y}px`;
+    element.style.transform = `scale(${scale})`;
     document.body.append(element);
   }
 }
