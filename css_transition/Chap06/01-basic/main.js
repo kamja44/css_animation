5b27550af4925bfcf554bba8d398f8b48d4c9dd0
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-05-03 23:19:27 +0900

    스크롤 타임라인 이해하기 2

--- css_transition/Chap06/01-basic/main.js
+++ css_transition/Chap06/01-basic/main.js
@@ -2,6 +2,7 @@ import "./scroll-timeline.js";
 
 const progress = document.querySelector(".progress");
 const scrollBox = document.querySelector(".scroll-box");
+const targetImages = document.querySelectorAll(".target-image");
 
 const keyframes = [{ transform: "scaleX(0)" }, { transform: "scaleX(1)" }];
 const options = {
@@ -23,3 +24,21 @@ const options = {
   }),
 };
 progress.animate(keyframes, options);
+
+targetImages.forEach((image) => {
+  // offset~을 이용하여 image의 위치를 가져올 수 있음
+  const imageTop = image.offsetTop;
+  const imageHeight = image.offsetHeight;
+
+  const offset1 = imageTop + imageHeight - window.innerHeight;
+  const offset2 = imageTop - 200;
+
+  image.animate([{ opacity: 0 }, { opacity: 1 }], {
+    timeline: new ScrollTimeline({
+      scrollOffsets: [
+        new CSSUnitValue(offset1, "px"),
+        new CSSUnitValue(offset2, "px"),
+      ],
+    }),
+  });
+});
